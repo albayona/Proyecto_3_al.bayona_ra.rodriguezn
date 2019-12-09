7c385ce6451b14b8f7fb765d39d248d88c248c75
Andres Bayona <al.bayona@uniandes.edu.co>
2019-12-08 21:28:31 -0500

    Changes

--- src/controller/main.java
+++ src/controller/main.java
@@ -1,8 +1,9 @@
 package controller;
 
+import model.data_structures.Graphs.UnexistingVertexException;
+
 public class main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws UnexistingVertexException {
 
         Controller controler = new Controller();
         controler.run();
--- src/view/MVCView.java
+++ src/view/MVCView.java
@@ -7,10 +7,8 @@ public class MVCView
 
 
 	public void printMenu() {
-		System.out.println("0. Cargar mundo");
-		System.out.println("1. Guarde el grafo en archivo Json");
-		System.out.println("2. Cargue el grafo de un archivo Json");
-		System.out.println("3. B1");
+		System.out.println("1. Cargar mundo");
+		System.out.println("1. B1");
 		System.out.println("4. B2");
 		System.out.println("5. B3");
 		System.out.println("6. C1");
